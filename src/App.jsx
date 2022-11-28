fa954c08242d0401a19bd30eead0814e280540ec
Reggi Sirilan <ahhreggi@gmail.com>
2022-11-27 18:43:07 -0700

    Add Stage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState, useEffect } from "react";
 // import { useSelector, useDispatch } from "react-redux";
 import "./App.scss";
+import Stage from "./components/Stage";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Connect Four</h1>
+      <Stage />
     </div>
   );
 };
---
+++ src/components/Stage.jsx
@@ -0,0 +1,7 @@
+import "./Stage.scss";
+
+const Stage = () => {
+  return <div className="Stage"></div>;
+};
+
+export default Stage;
